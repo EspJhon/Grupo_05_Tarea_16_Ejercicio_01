899a30920bae92a2891b731db2420547cd95c07f
MathhJ <matiarevalo611@gmail.com>
2024-07-14 19:34:10 -0500

    MatiasRamaV1

--- app/src/main/java/com/example/grupo_05_tarea_16_ejercicio_01/fragments/NormaDet/NormaDetFragment.java
+++ app/src/main/java/com/example/grupo_05_tarea_16_ejercicio_01/fragments/NormaDet/NormaDetFragment.java
@@ -1,6 +1,7 @@
 package com.example.grupo_05_tarea_16_ejercicio_01.fragments.NormaDet;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
@@ -13,12 +14,20 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.grupo_05_tarea_16_ejercicio_01.R;
 import com.example.grupo_05_tarea_16_ejercicio_01.adapter.NormaDetalleAdapter;
 import com.example.grupo_05_tarea_16_ejercicio_01.adapter.ZonaAdapter;
@@ -28,14 +37,20 @@ import com.example.grupo_05_tarea_16_ejercicio_01.modelo.Zona;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
-public class NormaDetFragment extends Fragment {
+public class NormaDetFragment extends Fragment implements Response.Listener<JSONObject>, Response.ErrorListener{
 
     DBHelper dbHelper;
     private Button btn_anadir_norma;
     private RecyclerView lvl_lista_norma;
     private NormaDetalleAdapter adapter;
+    ProgressDialog progreso;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -70,6 +85,7 @@ public class NormaDetFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_norma_det, container, false);
         dbHelper = new DBHelper(getActivity());
+        request = Volley.newRequestQueue(requireActivity());
         btn_anadir_norma = view.findViewById(R.id.btn_anadir_norma);
         lvl_lista_norma = view.findViewById(R.id.lvl_lista_norma);
         lvl_lista_norma.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -105,8 +121,19 @@ public class NormaDetFragment extends Fragment {
                     public void onClick(DialogInterface dialog, int which) {
                         String cnnorma = n_norma.getText().toString();
                         String cdescripcion = descripcion.getText().toString();
+
+
+                        if (cnnorma.isEmpty() || cdescripcion.isEmpty()) {
+                            Toast.makeText(getContext(), "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
                         NormasDet normasDet = new NormasDet(cnnorma, cdescripcion);
                         dbHelper.Insertar_Normas_Detalle(normasDet);
+
+                        Log.d("RegistroNorma", "N° de Norma: " + cnnorma + ", Nombre: " + cdescripcion);
+                        cargaWebService(cnnorma, cdescripcion);
+                        Toast.makeText(getContext(), "Norma registrada", Toast.LENGTH_SHORT).show();
                         Listar_Normas();
                     }
                 });
@@ -128,5 +155,33 @@ public class NormaDetFragment extends Fragment {
             }
         });
         lvl_lista_norma.setAdapter(adapter);
+    }
+
+    //WEB SERVICE
+    private void cargaWebService(String numnorma, String descripcion) {
+        progreso = new ProgressDialog(requireActivity());
+        progreso.setMessage("Cargando...");
+        progreso.show();
+
+        String url = "http://192.168.1.6/db_grupo_05_tarea_16_ejercicio_01/NormaRegistro.php?numnorma=" + numnorma + "&descripcion=" + descripcion;
+        url = url.replace(" ", "%20");
+
+        Log.d("URLWebService", "URL: " + url);
+
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+        request.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.dismiss();
+        Toast.makeText(requireActivity(), "No se puede conectar: " + error.toString(), Toast.LENGTH_LONG).show();
+        Log.d("ERROR: ", error.toString());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        progreso.dismiss();
+        Toast.makeText(requireActivity(), "Mensaje: " + response.toString(), Toast.LENGTH_SHORT).show();
     }
 }
